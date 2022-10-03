eb9e2c5af6adf678d5ea50c9bd1c2447157faee6
Pavel Yastremsky <goodieart@gmail.com>
2022-10-03 19:51:07 +0300

    task_038 completed

---
+++ task_038/Program.cs
@@ -0,0 +1,29 @@
+﻿using static System.Console;
+
+int[] array = ArrayCreate(5);
+ArrayPrint(array);
+Write(ArrayMinMaxDelta(array));
+
+int[] ArrayCreate(int size)
+{
+    int[] result = new int[size];
+    Random rnd = new Random();
+    for(int i = 0; i < result.Length; i++) result[i] = rnd.Next(0, 100);
+    return result;
+}
+
+void ArrayPrint(int[] array)
+{
+    WriteLine($"[{string.Join(",", array)}]");
+}
+
+int ArrayMinMaxDelta(int[] array)
+{
+    int min = array[0], max = array[0]; 
+    foreach(int i in array) 
+    {
+        if(i > max) max = i;
+        if(i < min) min = i;
+    }
+    return max - min;
+}
